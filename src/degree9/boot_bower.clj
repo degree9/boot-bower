e6eee255689a223a07448075932a9f730def70cc
Matthew Ratzke <matt@degree9.io>
2015-12-22 11:24:27 -0700

    sync wip

--- src/degree9/boot_bower.clj
+++ src/degree9/boot_bower.clj
@@ -10,28 +10,32 @@
 
 (boot/deftask bower
   "boot-clj wrapper for bower"
-  [i install   FOO=BAR {kw str} "Dependency map."
-   d directory VAL     str      "Directory to install components (defaults to 'bower_components' in target)"
-   g ignore    VAL     #{kw}    "List of packages to ignore."]
-  (let [deps    (or (:install   *opts*) "")
-        dir     (or (:directory *opts*) "/bower_components")
-        ignore  (or (:ignore    *opts*) "")
-        tmp     (boot/tmp-dir!)
-        tmpbwr  (boot/tmp-dir!)
-        bwrjson (generate-string {:name ::tmp :dependencies deps} {:pretty true})
-        bwrrc   (generate-string {:directory dir :ignoredDependencies ignore} {:pretty true})
-        jsonf   (io/file tmp "bower.json")
-        bwrrcf  (io/file tmp ".bowerrc")]
-    (comp
-     (boot/with-pre-wrap fileset
+  [i install     FOO=BAR {kw str} "Dependency map."
+   d directory   VAL     str      "Directory to install components (defaults to 'bower_components' in target)"
+   g ignore      VAL     #{kw}    "List of packages to ignore."
+   r resolutions VAL     {kw str} "Dependency resolutions."]
+  (let [deps         (or (:install     *opts*) "")
+        dir          (or (:directory   *opts*) "/bower_components")
+        ignore       (or (:ignore      *opts*) "")
+        res          (or (:resolutions *opts*) "")
+        tmp          (boot/tmp-dir!)
+        tmpbwr       (boot/tmp-dir!)
+        bwrjson      (generate-string {:name ::tmp :dependencies deps :resolutions res} {:pretty true})
+        bwrrc        (generate-string {:directory dir :ignoredDependencies ignore} {:pretty true})
+        jsonf        (io/file tmp "bower.json")
+        bwrrcf       (io/file tmp ".bowerrc")
+        local-bower  (io/as-file "./node_modules/bower/bin/bower")
+        global-bower (io/as-file "/usr/local/bin/bower")
+        bwrcmd       (cond (.exists global-bower) (.getPath global-bower)
+                           (.exists local-bower) (.getPath local-bower)
+                           :else "bower")]
+    (boot/with-pre-wrap fileset
        (doto jsonf  io/make-parents (spit bwrjson))
        (doto bwrrcf io/make-parents (spit bwrrc))
-       (let [local-bower (io/as-file "./node_modules/bower/bin/bower")
-            bwrcmd      (if (.exists local-bower) (.getPath local-bower) "bower")
-            cmdresult   @(exec/sh [bwrcmd "install" "--allow-root"] {:dir (.getPath tmp)})
-            exitcode    (:exit cmdresult)
-            errormsg    (:err cmdresult)]
-         (assert (= 0 exitcode) errormsg)
+       (let [cmdresult   @(exec/sh [bwrcmd "install" "--allow-root"] {:dir (.getPath tmp)})
+             exitcode    (:exit cmdresult)
+             errormsg    (:err cmdresult)]
+         (util/info (clojure.string/join ["Bower found at...: " bwrcmd "\n"]))
+         (assert (= 0 exitcode) (util/fail (clojure.string/join ["Bower failed with...: \n" errormsg "\n"])))
          (util/info "Bower run successful...\n")
-         (-> fileset (boot/add-resource tmp) boot/commit!)))
-     )))
+         (-> fileset (boot/add-resource tmp) boot/commit!)))))
