982930598f325c6af282701c1898d285c9d870b6
Matthew Ratzke <matt@degree9.io>
2016-08-07 14:29:37 -0600

    Auto Install Bower (#7)
    * integrate boot offline mode
    
    * update for auto-install bower

--- build.boot
+++ build.boot
@@ -4,7 +4,8 @@
                   [adzerk/bootlaces                    "0.1.13"]
                   [cheshire                            "5.5.0"]
                   [degree9/boot-semver                 "1.2.0"]
-                  [degree9/boot-exec                   "0.2.0"]]
+                  [degree9/boot-exec                   "0.2.0"]
+                  [degree9/boot-npm                    "0.2.0"]]
  :resource-paths   #{"src"})
 
 (require
@@ -13,11 +14,12 @@
 
 (task-options!
   pom {:project 'degree9/boot-bower
+       :version (get-version)
        :description "boot-clj task for wrapping bower"
        :url         "https://github.com/degree9/boot-bower"
        :scm         {:url "https://github.com/degree9/boot-bower"}})
 
-(deftask dev
+(deftask develop
   "Build boot-bower for development."
   []
   (comp
--- src/degree9/boot_bower.clj
+++ src/degree9/boot_bower.clj
@@ -1,30 +1,62 @@
 (ns degree9.boot-bower
-  {:boot/export-tasks true}
   (:require [boot.core :as boot]
+            [boot.task.built-in :as tasks]
             [boot.tmpdir :as tmpd]
             [boot.util :as util]
-            [degree9.boot-exec :as exec]
+            [cheshire.core :refer :all]
             [clojure.java.io :as io]
-            [boot.task.built-in :as tasks]
-            [cheshire.core :refer :all]))
+            [degree9.boot-exec :as exec]
+            [degree9.boot-npm :as npm]))
+
+(defn deep-merge
+  "Like merge, but merges maps recursively."
+  [& maps]
+  (cond (every? map? maps) (apply merge-with deep-merge maps)
+        (nil? (last maps)) (apply deep-merge (butlast maps))
+        :else (last maps)))
+
+(defn file->json [file]
+  (let [file (boot/tmp-file file)]
+    (parse-string (slurp file) true)))
+
+(defn maps->str [& maps]
+  (generate-string (apply deep-merge maps) {:pretty true}))
+
+(defn spit-file [file path contents]
+  (doto (io/file path file)
+        io/make-parents
+        (spit contents)))
 
 (boot/deftask bower
   "boot-clj wrapper for bower"
-  [i install     FOO=BAR {kw str} "Dependency map."
+  [b bower       VAL     str      "A bower.json file to parse."
+   w bowerrc     VAL     str      "A .bowerrc file to parse."
+   i install     FOO=BAR {kw str} "Dependency map."
    d directory   VAL     str      "Directory to install components (defaults to 'bower_components' in target)"
    g ignore      VAL     #{kw}    "List of packages to ignore."
    r resolutions VAL     {kw str} "Dependency resolutions."
    c cache-key   VAL     kw       "Optional cache key for when bower is used with multiple dependency sets."]
-  (let [deps         (:install     *opts* "")
-        dir          (:directory   *opts* "/bower_components")
-        ignore       (:ignore      *opts* "")
-        res          (:resolutions *opts* "")
-        cache-key    (:cache-key   *opts*)
-        tmp          (if cache-key (boot/cache-dir! cache-key) (boot/tmp-dir!))
-        tmp-path     (.getAbsolutePath tmp)
-        bwrjson      (generate-string {:name ::tmp :dependencies deps :resolutions res} {:pretty true})
-        bwrrc        (generate-string {:directory dir :ignoredDependencies ignore} {:pretty true})]
+  (let [bwrjsonf  (:bower       *opts* "./bower.json")
+        bwrrcf    (:bowerrc     *opts* "./.bowerrc")
+        deps      (:install     *opts*)
+        dir       (:directory   *opts* "bower_components")
+        ignore    (:ignore      *opts*)
+        res       (:resolutions *opts*)
+        cache-key (:cache-key   *opts* ::cache)
+        tmp       (boot/cache-dir! cache-key)
+        tmp-path  (.getAbsolutePath tmp)
+        bwrjson   {:name ::tmp :dependencies deps :resolutions res}
+        bwrrc     {:directory dir :ignoredDependencies ignore}
+        args      ["install" "--allow-root"]
+        args      (if (boot/get-env :offline?) (conj args "--offline") args)]
     (comp
-      (exec/properties :contents bwrjson :directory tmp-path :file "bower.json")
-      (exec/properties :contents bwrrc :directory tmp-path :file ".bowerrc")
-      (exec/exec :process "bower" :arguments ["install" "--allow-root"] :directory tmp-path :local "node_modules/bower/bin"))))
+      (boot/with-pass-thru fileset
+        (let [bwrjsonfs (boot/tmp-get fileset bwrjsonf)
+              bwrrcfs (boot/tmp-get fileset bwrrcf)]
+          (if bwrjsonfs
+            (spit-file (-> bwrjsonfs boot/tmp-file .getName) tmp (maps->str (file->json bwrjsonfs) bwrjson))
+            (spit-file bwrjsonf tmp (maps->str bwrjson)))
+          (if bwrrcfs
+            (spit-file (-> bwrrcfs boot/tmp-file .getName) tmp (maps->str (file->json bwrrcfs) bwrrc))
+            (spit-file bwrrcf tmp (maps->str bwrrc)))))
+      (npm/exec :module "bower" :arguments args :cache-key cache-key))))
